b60782b9256ef5e860b14905c178281b20c3a759
TrentJ7 <120288441+TrentJ7@users.noreply.github.com>
2025-04-14 22:08:47 -0400

    Changed Config file

--- solar_site/next.config.ts
+++ solar_site/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // allow all domains (not recommended for production)
+      },
+    ],
+  },
 };
 
 export default nextConfig;
